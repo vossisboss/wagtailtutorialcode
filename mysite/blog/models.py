5bb8936a4529efb6fecfa3ae129c2070ec5f0b0e
vossisboss <vossmk@gmail.com>
2024-01-23 15:46:30 -0500

    Added BlogPage model.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-# Add these:
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
 
 
 class BlogIndexPage(Page):
@@ -11,4 +11,21 @@ class BlogIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro')
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body'),
     ]
