04065ba585a3a103b0451363abf98852a44d9b27
vossisboss <vossmk@gmail.com>
2024-01-23 15:18:47 -0500

    Added BlogIndexPage and migrations.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+# Add these:
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+from wagtail.admin.panels import FieldPanel
+
+
+class BlogIndexPage(Page):
+    intro = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('intro')
+    ]
