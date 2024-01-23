04d2b83cc9af8fa1c62f22203eca7a9a02e65634
vossisboss <vossmk@gmail.com>
2024-01-23 15:00:44 -0500

    Output generated from the Wagtail Start Command.

---
+++ mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--r^h2v2%d7otwaxpyf2i2&u^897@ypx^=g9^5ctm!#hs%2qaol"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
