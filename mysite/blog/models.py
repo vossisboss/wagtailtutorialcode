012839abb16a02ef8d5adf49e36e5b0ee795dc61
vossisboss <vossmk@gmail.com>
2024-01-26 15:54:05 -0500

    Fixed another filter issue.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,13 +1,9 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField, StreamField
+from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
 from wagtail.search import index
-from wagtail import blocks
-from wagtail.images.blocks import ImageChooserBlock
-from wagtail.embeds.blocks import EmbedBlock
-
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -20,12 +16,7 @@ class BlogIndexPage(Page):
 class BlogPage(Page):
     date = models.DateField("Post date")
     intro = models.CharField(max_length=250)
-    body = StreamField([
-        ('heading', blocks.CharBlock(form_classname="title")),
-        ('paragraph', blocks.RichTextBlock()),
-        ('image', ImageChooserBlock()),
-        ('embed', EmbedBlock(max_width=800, max_height=400)),
-    ], use_json_field=True)
+    body = 
 
     search_fields = Page.search_fields + [
         index.SearchField('intro'),
